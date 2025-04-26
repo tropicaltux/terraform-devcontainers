36490ecd66ae0dc400f1611e373cc607582aa9ef
tropicaltux <tropicaltux@proton.me>
2025-04-26 13:21:52 +0000

    Added multiple devcontainer example.

---
+++ examples/aws/ec2/multiple-devcontainers/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "devcontainers" {
+  source = "github.com/tropicaltux/terraform-devcontainers"
+  devcontainers = [
+    {
+      source = "https://github.com/microsoft/vscode-remote-try-python.git"
+    },
+    {
+      source = "https://github.com/microsoft/vscode-remote-try-node.git"
+    },
+    {
+      source = "https://github.com/microsoft/vscode-remote-try-go.git"
+    }
+  ]
+  public_key_path = "~/.ssh/id_ed25519.pub"
+  instance_type   = "t2.micro"
+}
+
+output "devcontainer_module_output" {
+  value = module.devcontainers
+}
