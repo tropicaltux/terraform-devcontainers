f0c91190669ca074266870fb6535342b8033ccc2
tropicaltux <tropicaltux@proton.me>
2025-05-03 18:39:42 +0300

    Move key pairs and ssm to a new files.

---
+++ key_pair.tf
@@ -0,0 +1,17 @@
+resource "aws_key_pair" "this" {
+  count = local.should_create_key_pair ? 1 : 0
+
+  key_name   = local.key_pair_name
+  public_key = local.should_create_key_pair ? file(var.public_ssh_key.local_key_path) : null
+}
+
+# Get container-specific AWS Key Pairs if aws_key_pair_name is specified
+data "aws_key_pair" "container_specific" {
+  for_each = {
+    for i, c in local.prepared_devcontainers : tostring(i) => c.remote_access.ssh.public_ssh_key.aws_key_pair_name
+    if try(c.remote_access.ssh, null) != null &&
+    try(c.remote_access.ssh.public_ssh_key.aws_key_pair_name, null) != null
+  }
+
+  key_name = each.value
+}
---
+++ ssm.tf
@@ -0,0 +1,33 @@
+# Put individual SSH keys for devcontainers that specify their own keys into SSM Parameter Store
+resource "aws_ssm_parameter" "container_ssh_public_keys" {
+  for_each = {
+    for i, c in local.prepared_devcontainers : tostring(i) => c
+    if try(c.remote_access.ssh, null) != null
+  }
+
+  name        = "/${var.name}/devcontainers/${each.value.id}/ssh-public-key"
+  description = "SSH public key for devcontainer ${each.value.id}"
+  type        = "SecureString"
+  value       = try(each.value.remote_access.ssh.public_ssh_key.local_key_path, null) != null ? file(each.value.remote_access.ssh.public_ssh_key.local_key_path) : data.aws_key_pair.container_specific[each.key].public_key
+}
+
+# Put generated random tokens for each devcontainer into SSM Parameter Store
+resource "aws_ssm_parameter" "openvscode_tokens" {
+  for_each = {
+    for i, c in local.prepared_devcontainers : tostring(i) => c
+    if try(c.remote_access.openvscode_server, null) != null
+  }
+
+  name        = "/${var.name}/devcontainers/${each.value.id}/openvscode-token"
+  description = "OpenVSCode Server token for devcontainer ${each.value.id}"
+  type        = "SecureString"
+  value       = random_password.tokens[each.key].result
+}
+
+# Generate random tokens for each devcontainer
+resource "random_password" "tokens" {
+  for_each = { for i, c in local.prepared_devcontainers : tostring(i) => c }
+
+  length  = 32
+  special = false
+}
